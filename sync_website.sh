8c3e62f0d3f93f7e739e5ef4d9aaa560909c0430
Taha Derouiche <taha,derouiche@gmail.com>
2016-01-24 12:47:31 +0800

    added error html file

--- sync_website.sh
+++ sync_website.sh
@@ -1,1 +1,1 @@
-aws s3 sync ~/Work/tahaderouiche.com/ s3://tahaderouiche.com --exclude "*.sh" --exclude "*.md"
+aws s3 sync ~/Work/tahaderouiche.com/ s3://tahaderouiche.com --exclude "*.sh" --exclude "*.md" --exclude "README" --exclude ".git/*" --exclude "LICENSE" --delete
